19bf239ed485f4d89cca78680f25e8c36fa1b772
bargitta <bargittachen@gmail.com>
2014-04-21 21:19:16 +0800

    add  method call and function call invocation patterns

--- function.js
+++ function.js
@@ -8,7 +8,7 @@ var add = function (a,b){
 //method invocation pattern
 //function is a property of an object
 var talk = {
-	content: '',
+	content: 'content',
 	start: function(input){
 		this.content = input;
 		console.log(this.content);
@@ -16,10 +16,37 @@ var talk = {
 	end: function(){
 		this.content = '';
 		console.log('end');
+	},
+	memo: function(){
+		console.log(this.content);
 	}
 };
 
-talk.start('avc');
-talk.end();
+//talk.start('avc');//this points to talk
+//talk.end();
 
-//
+//function invocation pattern
+var rusult = add(3,4);//this points to global
+
+
+//use inner function
+talk.interrupt = function(){
+
+	var call = function(){
+		this.content = '1234';
+	};
+	call();//this points to global
+};
+
+talk.interruptInner = function(){
+	var that = this; //cache this
+	var call = function(){
+		that.content = '1234';
+	};
+	call();
+};
+
+talk.interrupt();
+talk.memo();
+talk.interruptInner();
+talk.memo();
