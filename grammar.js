6c049d94bfae66ed80cd3d471f48fb9d9c8d3094
bargitta <bargittachen@gmail.com>
2014-06-11 21:02:38 +0800

    add grammer.js

---
+++ grammar.js
@@ -0,0 +1,41 @@
+//expression
+
+var josh = {
+	year: '1999-01-31',
+	color: 'blue', 
+	weight: 12
+}
+
+console.assert(josh.year);
+delete josh.color;
+console.assert(josh.color === undefined);
+
+//falsy values in javascript
+//undefined, 0, NaN, false, '', null
+
+console.assert(typeof(NaN) === 'number')
+console.assert(typeof(Infinity) === 'number')
+
+//logical operators 
+//&& 
+//when the first operator is false, return the first
+//when the first is true, return the second
+var a = true && true;
+var b = true && false;
+var c = false && false;
+var d = false && true;
+
+console.assert(a);
+console.assert(!b);
+console.assert(!c);
+console.assert(!d);
+
+var e = 0 && 'a';
+console.assert(!e);
+
+var d = 'a'&&'b';
+console.assert(d === 'b');
+
+
+
+
